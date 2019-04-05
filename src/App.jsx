496bf2ab2544c240375703e18c937d29067e8b13
vaxevanis <vaxevanis.paul@gmail.com>
2019-04-05 13:05:04 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+
+function VolumeController({ initialVolume }) {
+  const [volume, setVolume] = useState(initialVolume);
+
+  return (
+    <div className="input-group">
+      <input className="form-control m-2" value={volume} size={3} />
+      <div className="input-group-append">
+        <button
+          className="btn btn-warning m-2"
+          onClick={() => setVolume(initialVolume)}
+        >
+          Reset
+        </button>
+        <button
+          className="btn btn-secondary m-2"
+          onClick={() => setVolume(volume - 1)}
+        >
+          + Vol
+        </button>
+        <button
+          className="btn btn-secondary m-2"
+          onClick={() => setVolume(volume - 1)}
+        >
+          - Vol
+        </button>
+      </div>
+    </div>
+    );
+     
+
+}; 
+
+
+export default VolumeController;
